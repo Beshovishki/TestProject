c8491232cb58dc62e09464d70704f5029f0ce2b0
Beshovishki <atanas.beshovishki@gmail.com>
2025-03-06 11:02:51 +0200

    test 733

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Test h1"
+        ?>
+    </h1>
+    <h2>
+        <?php 
+        echo "Test h2"
+        ?>
+    </h2>
+    <h1>
+        <?php 
+        echo "Test h1"
+        ?>
+    </h1>
+</body>
+</html>
